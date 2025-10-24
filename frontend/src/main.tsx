1e5c9b753158df60deeae0ec2078d1cf3cd61047
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-10-24 22:30:41 +0300

    add login page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Dashboard from "./pages/Dashboard.tsx";
+import "./index.css";
 import Customer from "./pages/Customer.tsx";
+import Dashboard from "./pages/Dashboard.tsx";
+import Login from "./pages/Login.tsx";
 import ManageStaff from "./pages/ManageStaff.tsx";
 import Orders from "./pages/Orders.tsx";
 import Products from "./pages/Products.tsx";
 import Reports from "./pages/Reports.tsx";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Dashboard /> },
+  { path: "/", element: <Login /> },
+  { path: "/login", element: <Login /> },
   { path: "/dashboard", element: <Dashboard /> },
   { path: "/customer", element: <Customer /> },
   { path: "/products", element: <Products /> },
---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,44 @@
+import loginImg from "../assets/login-img.jpg";
+import bakeryLogo from "../assets/bakery-logo.png";
+import cupcakeIcon from "../assets/icon-cupcake.svg";
+import mailIcon from "../assets/icon-mail.svg";
+import lockIcon from "../assets/icon-lock.svg";
+
+export default function Login() {
+  return (
+    <div className="flex">
+      <img
+        src={bakeryLogo}
+        className="absolute -mt-12 -ml-5 filter-[brightness(0)_saturate(100%)_invert(36%)_sepia(14%)_saturate(1035%)_hue-rotate(1deg)_brightness(94%)_contrast(91%)]"
+      />
+      <img src={loginImg} className="h-screen w-[45%] object-cover object-center" />
+      <div className="flex justify-center flex-col items-center w-[55%]">
+        <div className="title flex items-center gap-4">
+          <img src={cupcakeIcon} className="w-20" />
+          <h1 className="text-[60px] font-bold tracking-wide">Bakery</h1>
+        </div>
+        <p className="mt-4 text-[48px] font-semibold tracking-widest">Welcom back!</p>
+        <form className="min-w-[40%] max-w-[500px] mt-20 flex flex-col gap-6">
+          <div className="border border-[#ABA89E] p-5 w-full flex gap-8 rounded-[10px] overflow-clip">
+            <img src={mailIcon} />
+            <input type="email" placeholder="Email" className="text-[28px] outline-0 caret-[#ABA89E]" />
+          </div>
+          <div className="border border-[#ABA89E] p-5 w-full flex gap-8 rounded-[10px] overflow-clip">
+            <img src={lockIcon} />
+            <input
+              type="password"
+              placeholder="Password"
+              className="text-[28px] outline-0 caret-[#ABA89E] block flex-1"
+            />
+          </div>
+          <button
+            type="submit"
+            className="p-5 w-full rounded-[10px] bg-[#FFB74A] text-white text-[28px] text-center cursor-pointer"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
