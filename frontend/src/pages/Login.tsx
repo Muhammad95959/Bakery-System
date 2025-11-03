f2c7bebe940c171c9600c67f49a19319c590e75a
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-11-03 19:36:51 +0200

    link the login page with the backend

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,10 +3,34 @@ import bakeryLogo from "../assets/bakery-logo.png";
 import cupcakeIcon from "../assets/icon-cupcake.svg";
 import mailIcon from "../assets/icon-mail.svg";
 import lockIcon from "../assets/icon-lock.svg";
+import { BACKEND_URL } from "../constants";
+import axios from "axios";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 
 export default function Login() {
+  const navigate = useNavigate();
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  async function login(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    await axios
+      .post(`${BACKEND_URL}/auth/login`, {
+        username: usernameRef.current?.value,
+        password: passwordRef.current?.value,
+      })
+      .then((res) => {
+        if (res.data.data.user.role.toLowerCase() === "admin") navigate("/manage-staff");
+        else navigate("/orders");
+      })
+      .catch((err) => toast.error(err.response.data.message));
+  }
+
   return (
     <div className="flex bg-[#FFFCEF]">
+      <ToastContainer position="bottom-center" autoClose={3000} />
       <img
         src={bakeryLogo}
         className="absolute -mt-12 -ml-5 filter-[brightness(0)_saturate(100%)_invert(36%)_sepia(14%)_saturate(1035%)_hue-rotate(1deg)_brightness(94%)_contrast(91%)]"
@@ -18,17 +42,25 @@ export default function Login() {
           <h1 className="text-[60px] font-bold tracking-wide">Bakery</h1>
         </div>
         <p className="mt-4 text-[48px] font-semibold tracking-widest">Welcom back!</p>
-        <form className="min-w-[40%] max-w-[500px] mt-20 flex flex-col gap-6">
-          <div className="border border-[#ABA89E] p-5 w-full flex gap-8 rounded-[10px] overflow-clip">
-            <img src={mailIcon} />
-            <input type="text" placeholder="Username" className="text-[28px] outline-0 caret-[#ABA89E]" />
+        <form onSubmit={login} className="min-w-[40%] max-w-[500px] mt-20 flex flex-col gap-6">
+          <div className="border border-[#ABA89E] p-5 w-full flex gap-8 rounded-[10px]">
+            <img src={mailIcon} className="w-10" />
+            <input
+              required
+              type="text"
+              placeholder="Username"
+              className="text-[28px] outline-0 caret-[#ABA89E] w-full"
+              ref={usernameRef}
+            />
           </div>
           <div className="border border-[#ABA89E] p-5 w-full flex gap-8 rounded-[10px] overflow-clip">
-            <img src={lockIcon} />
+            <img src={lockIcon} className="w-10" />
             <input
+              required
               type="password"
               placeholder="Password"
-              className="text-[28px] outline-0 caret-[#ABA89E] block flex-1"
+              className="text-[28px] outline-0 caret-[#ABA89E] block flex-1 w-full"
+              ref={passwordRef}
             />
           </div>
           <button
