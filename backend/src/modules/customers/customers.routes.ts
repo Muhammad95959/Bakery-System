43949de5003eb726ac4c9a5a0edca817a4ab1845
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-11-05 19:59:02 +0200

    update customers routes

--- backend/src/modules/customers/customers.routes.ts
+++ backend/src/modules/customers/customers.routes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { Role } from "../../generated/prisma/enums";
 import * as authController from "../auth/auth.controller";
 import * as customersController from "./customers.controller";
 
@@ -10,8 +11,8 @@ router.get("/:id", authController.protect, customersController.getCustomerById);
 
 router.post("/", authController.protect, customersController.createCustomer);
 
-router.put("/:id", authController.protect, customersController.updateCustomer);
+router.put("/:id", authController.protect, authController.restrict(Role.ADMIN), customersController.updateCustomer);
 
-router.delete("/:id", authController.protect, customersController.deleteCustomer);
+router.delete("/:id", authController.protect, authController.restrict(Role.ADMIN), customersController.deleteCustomer);
 
 export default router;
