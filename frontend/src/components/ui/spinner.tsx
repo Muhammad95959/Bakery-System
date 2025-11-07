673e2c9e31894f7bad3f9a4b508823bc75d1a46d
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-11-07 05:57:31 +0200

    fix the spinner

--- frontend/src/components/ui/spinner.tsx
+++ frontend/src/components/ui/spinner.tsx
@@ -1,7 +1,7 @@
 export default function Spinner() {
   return (
-    <div className="spinner flex items-center justify-center h-screen w-full opacity-0">
-      <div className="animate-spin rounded-full h-16 w-16 border-5 border-[#FFAC3E] border-l-transparent"></div>
+    <div className="spinner-container flex items-center justify-center h-screen w-full opacity-0">
+      <div className="spinner rounded-full h-16 w-16 border-5 border-[#FFAC3E] border-l-transparent"></div>
     </div>
   );
 }
