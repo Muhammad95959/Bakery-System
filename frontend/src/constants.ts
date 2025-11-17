366602a1487376bcbf8219b27acbc31bb9258d7f
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-11-17 20:01:54 +0200

    change the backend url

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://localhost:7373/api";
+export const BACKEND_URL = "https://bakery-system-backend-nine.vercel.app/api";
 
 export const paymentMethods = ["CASH", "CREDIT_CARD", "DEBIT_CARD", "BANK_TRANSFER", "MOBILE_WALLET", "ONLINE_PAYMENT"];
 
