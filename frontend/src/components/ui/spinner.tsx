6f83562362a3c8ee7e662083df139b88205bcfdf
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-11-18 08:51:32 +0200

    fix sidebar flickering caused by the spinner

--- frontend/src/components/ui/spinner.tsx
+++ frontend/src/components/ui/spinner.tsx
@@ -1,6 +1,6 @@
 export default function Spinner() {
   return (
-    <div className="spinner-container flex items-center justify-center h-screen w-full opacity-0">
+    <div className="spinner-container flex items-center justify-center h-screen basis-[80%] opacity-0">
       <div className="spinner rounded-full h-16 w-16 border-5 border-[#FFAC3E] border-l-transparent"></div>
     </div>
   );
